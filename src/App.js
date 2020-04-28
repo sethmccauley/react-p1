ddf572d582999e2bf7c37f79289d52ef23e5e285
Seth McCauley <seth.a.mccauley@gmail.com>
2020-04-28 10:49:54 -0600

    Tiny counter push.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(10);
+
+  function myOnClick(e) {
+    setCount(count + 1)
+    console.log('Clicked.', count)
+  }
+
   return (
     <div className="App">
+      <div>
+        <h2 onClick={myOnClick}>Hello World {count}</h2>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
